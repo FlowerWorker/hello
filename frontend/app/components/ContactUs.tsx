2cf6980a973f244f7324b00b2a0ada1d82ecdbb3
RonaZong <ronazong@gmail.com>
2025-01-24 19:32:53 +0100

    Form

--- frontend/app/components/ContactUs.tsx
+++ frontend/app/components/ContactUs.tsx
@@ -3,8 +3,10 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import ReactQuill from 'react-quill';
-import { Footer, InfoPageHeader } from "./layout"
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
+// import ReactQuill from 'react-quill';
+import { Navbar, Footer, InfoPageHeader } from "./layout"
 import logo from "../public/LOGO_on_white.png";
 import TiktokIcon from "./icons/TiktokIcon";
 import InstagramIcon from "./icons/InstagramIcon";
@@ -15,25 +17,31 @@ import TwitterIcon from "./icons/TwitterIcon";
 const ContactUs = () => {
     return (
         <div className="w-full bg-bgdarkv1 text-white text-base ">
-            <header>
-                <InfoPageHeader title={"Contact Us"}/>
+            {/* Header Section */}
+            <header className="relative z-30">
+                <Navbar className="z-40" />
             </header>
-            
-            <div className="max-w-full m-8">
-                <p className="text-2xl font-regular font-opensans">
-                    We're here to help! Whether you have questions, feedback, or need assistance, our team is ready to support you. Reach out to us anytime, and we’ll ensure you get the answers and solutions you need. Let’s connect and make great things happen together!
-                </p>
-            </div>
 
-            <div className="max-w-full bg-white text-black rounded-lg m-8">
-                <div className="flex flex-row md:flex-col m-8">
-                    {/* Logo Section */}
-                    <ContactInfoSection />
-                    {/* Form Field */}
-                    <ContactFormSection />
+            <main className="flex flex-col w-full max-w-[1920px] mx-auto h-auto px-[2%] md:px-[12%] py-[80px] gap-[64px]">
+                <div className="container mx-auto">
+                    <InfoPageHeader title={"Contact Us"}/>
                 </div>
-            </div>
+                <div className="max-w-full m-8">
+                    <p className="text-2xl font-regular font-opensans">
+                        We're here to help! Whether you have questions, feedback, or need assistance, our team is ready to support you. Reach out to us anytime, and we’ll ensure you get the answers and solutions you need. Let’s connect and make great things happen together!
+                    </p>
+                </div>
 
+                <div className="max-w-full bg-white text-black rounded-lg m-8">
+                    <div className="flex flex-row md:flex-row sm:flex-col m-8">
+                        {/* Logo Section */}
+                        <ContactInfoSection />
+                        {/* Form Field */}
+                        <ContactFormSection />
+                    </div>
+                </div>
+            </main>
+            
             {/* Footer */}
             <footer>
                 <Footer />
@@ -85,30 +93,30 @@ const ContactFormSection = () => {
     return (
         <div className="flex-[1] flex flex-col justify-center justify-items-center content-center items-center space-y-3">
             <p className="text-lg font-regular font-opensans">Don't hesitate to reach out! We're happy to answer any questions you have, no matter how big or small.</p>
-            <form className="w-full max-w-6xl mx-auto grid-inline grid-cols-1 sm:grid-cols-2 gap-4 px-4 sm:px-6 mb-10">
+            <form className="w-full max-w-6xl mx-auto grid-inline gap-4 px-4 sm:px-6 mb-10">
                 {/* Form Inputs */}
-                <div className="col-span-2">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                        <div className="flex flex-col">
-                            <label className="block mb-2 text-sm text-black">
-                                Name
-                            </label>
-                            <input type="text" placeholder="Enter your name..." className="px-4 py-2 bg-white text-black border border-gray-300 rounded-md"/>
-                        </div>
-                        <div className="flex flex-col">
-                            <label className="block mb-2 text-sm text-black">
-                                Surname
-                            </label>
-                            <input type="text" placeholder="Enter your surname..." className="px-4 py-2 bg-white text-black border border-gray-300 rounded-md"/>
-                        </div>
+                <div className="grid grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4">
+                    <div className="flex flex-col">
+                        <label className="block mb-2 text-sm text-black">
+                            Name
+                        </label>
+                        <input type="text" placeholder="Enter your name..." className="px-4 py-2 bg-white text-black border border-gray-300 rounded-md"/>
                     </div>
-                    <div className="mt-6"> 
+                    <div className="flex flex-col">
+                        <label className="block mb-2 text-sm text-black">
+                            Surname
+                        </label>
+                        <input type="text" placeholder="Enter your surname..." className="px-4 py-2 bg-white text-black border border-gray-300 rounded-md"/>
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 gap-4">
+                    <div className="mt-6 flex flex-col"> 
                         <label className="block mb-2 text-sm text-black">
                             Phonenumber (optional)
                         </label>
                         <input type="tel" placeholder="Enter your phonenumber..." className="px-4 py-2 bg-white text-black border border-gray-300 rounded-md"/>
                     </div>
-                    <div className="mt-8">
+                    <div className="mt-8 flex flex-col">
                         <label className="block mb-2 text-sm text-black">
                             Email
                         </label>
@@ -118,16 +126,21 @@ const ContactFormSection = () => {
                         <label className="block mb-2 text-sm text-black">
                             Write us a message
                         </label>
-                        <ReactQuill value={content} onChange={handleChange} />
+                        <ReactQuill/>
+                        {/* <ReactQuill value={content} onChange={handleChange} /> */}
                     </div>
                 </div>
+                
             </form>
         </div>
     )
 }
 
-const RichTextEdito = () => {
-    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+const ReactQuill = dynamic(() => import("react-quill"), {srr: false});
+// const RichTextEditor = () => {
+//     const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
-}
+//     return <ReactQuill theme="snow"/>
+
+// }
 export default ContactUs;
