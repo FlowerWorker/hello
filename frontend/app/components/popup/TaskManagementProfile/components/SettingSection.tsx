7218edd06abae77a414dd3b7c068c37b5ccc2cc4
RonaZong <ronazong@gmail.com>
2025-02-10 18:24:47 +0100

    Update Task-Management with latest develop

---
+++ frontend/app/components/popup/TaskManagementProfile/components/SettingSection.tsx
@@ -0,0 +1,21 @@
+import ProfileSettings from '@/app/components/popup/TaskManagementProfile/components/settings/page';
+import { SettingsSectionProps } from '@/app/components/popup/TaskManagementProfile/hooks';
+
+export const SettingsSection: React.FC<SettingsSectionProps> = ({ isSettingsOpen, toggleSettings }) => {
+    return (
+        <div className='flex flex-col items-start gap-3'>
+            {/* Settings Popup */}
+            <button onClick={toggleSettings} className='text-xl font-medium font-mono hover:underline'>
+                Settings
+            </button>
+            {isSettingsOpen && (
+                <ProfileSettings toggleSettings={toggleSettings} />
+            )}
+
+            {/* Logout Section */}
+            <button className='text-xl font-medium font-mono hover:underline'>
+                Log out of FlowerWorker
+            </button>
+        </div>
+    )
+}
