99533da1cb0e34c83063d009654396caf7f96fdd
Annmaryjoppan <annmaryjoppan1998@gmail.com>
2025-04-14 12:09:24 +0200

    task priority completed

--- frontend/app/components/ui/SelectPriority.tsx
+++ frontend/app/components/ui/SelectPriority.tsx
@@ -43,7 +43,7 @@ export default function SelectPriority({ togglePriority, onSelectPriority }: Sel
           </div>
           <button
             onClick={togglePriority}
-            aria-label="Close priority modal"
+            aria-label ="Close priority modal"
             className="focus:outline-none"
           >
             <IoClose className="w-5 h-5 text-gray-500 hover:text-gray-700" />
